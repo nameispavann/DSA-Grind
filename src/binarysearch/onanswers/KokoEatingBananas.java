796cdaae53a32689c26bdb495ed9a8b4f70cf055
nameispavann <nameispavan2.0@gmail.com>
2025-07-12 22:55:18 +0530

    Binary Search - 12/07/2025

--- src/binarysearch/onanswers/KokoEatingBananas.java
+++ src/binarysearch/onanswers/KokoEatingBananas.java
@@ -3,6 +3,32 @@ package binarysearch.onanswers;
 import static binarysearch.onanswers.SmallestDivisor.calculateSum;
 import static binarysearch.onanswers.SmallestDivisor.findMax;
 
+/**
+ * The {@code KokoEatingBananas} class solves the well-known problem of determining the minimum eating rate
+ * (bananas per hour) required for Koko to finish all banana piles within a given number of hours.
+ * <p>
+ * This is a classic application of the "binary search on answers" technique where the goal is to minimize
+ * the maximum rate (optimization). Given an array representing the number of bananas in each pile and a
+ * total time limit (in hours), the program computes the minimum integer rate at which Koko can eat such
+ * that all piles are consumed within the time limit.
+ * <p>
+ * The search space lies between 1 and the size of the largest pile, and binary search is applied over
+ * this range. For a given mid-point (rate), a helper method determines whether Koko can finish all bananas
+ * within the limit. The algorithm recursively narrows the search range to find the optimal rate.
+ * <p>
+ * In this problem we try to <b>minimize the maximum</b>
+ *
+ * <p><b>Example Use Case:</b></p>
+ * <pre>{@code
+ * int[] piles = {1, 2, 3, 4, 5};
+ * int timeLimit = 8;
+ *
+ * int result = minimumRateToEatBananas(piles, timeLimit, 1, findMax(piles));
+ * System.out.println(result); // Outputs the minimum eating rate per hour
+ * }</pre>
+ *
+ * @author Sai Pavan
+ */
 public class KokoEatingBananas {
     public static void main(String[] args) {
         int low = 1;
